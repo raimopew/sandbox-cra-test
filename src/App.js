0083af712ac2816fde633e8ee53fa717b8c765d4
Raimo Pregel <raimo.pregel@gmail.com>
2022-09-03 20:07:52 +0000

    Test1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import "./styles.css";
+import { useState } from "react"
+import "./styles.css"
 
 export default function App() {
+  const [message, setMessage] = useState("")
+    const [number, setNumber] = useState(0)
+    const [show, setShow] = useState(false)
+
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Anastasia</h1>
+      <h2>Hi zomg</h2>
+      <Hello hi={"hello"} god={"mode"}/> <br/>
+      <div>
+        <button onClick={() => setMessage("Pressed button 1")}>Button1</button>
+        <button onClick={() => setMessage("Pressed button 2")}>Button2</button>
+      </div>
+      <div >
+        <button onClick={() => setNumber(prevNumber => prevNumber + 1)}>Add 1</button>
+        <button onClick={() => setNumber(prevNumber => prevNumber - 1)}>Substract 1</button>
+      </div>
+      
+      <p>{message}</p>
+      <p>{number}</p>
+      <button onClick={() => setShow(prevShow => !prevShow)}>Toggle show</button>
+      <Popup show={show}/>
     </div>
-  );
+  )
 }
+
+const Hello = (props) => {
+  return JSON.stringify(props, null, 2)
+}
+
+const Popup = ({ show }) => show && <p>Lol showing</p>
